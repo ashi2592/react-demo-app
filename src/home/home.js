3abe822256bfbc52de87822b708e6b46187a5037
ashi2592 <ashishsrivastava4u.09@gmail.com>
2024-04-25 11:00:39 +0530

    add react app

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+const Home = (props) =>{
+ return (<div className="mainContainer">
+    <div className="titleContainer">
+        <div>welcome Ashish</div>
+    </div>
+    <div className="bodyContainer">
+        <div className="button conatiner">
+
+        </div>
+    </div>
+ </div>)
+}
